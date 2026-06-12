89111389c1dace07fbf8741c7645d8d3a9cecdd7
Imran Shafiq <imranshafiq0605@gmail.com>
2026-06-12 11:51:50 +0200

    Add firebase setup

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9s6X8IzsNGXRe0__HzRnHl8V8IQbq1zE",
-  authDomain: "sm-dienstleistung-f0873.firebaseapp.com",
-  projectId: "sm-dienstleistung-f0873",
-  storageBucket: "sm-dienstleistung-f0873.firebasestorage.app",
-  messagingSenderId: "541172763743",
-  appId: "1:541172763743:web:334ba419316ef88bed9fea",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
